9567a05e323a159b36e8542dcf7e699767496483
AyushDeveloper <debayush2002@gmail.com>
2025-01-21 21:10:57 +0530

    updated helper

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 require('dotenv').config(); // Import dotenv to load the environment variables
 const v1Router = require('./src/Routes/v1.routes');
 const app = express();
+const Config = require('../Configs/Config.json');
 
 // Use the environment variables
 const mongoUri = process.env.MONGODB_URL;
