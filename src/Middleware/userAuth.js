ca4358e76e19308e24d5c83f98e5faa369b9a2d7
AyushDeveloper <debayush2002@gmail.com>
2025-01-22 10:26:21 +0530

    more config changes to env

--- src/Middleware/userAuth.js
+++ src/Middleware/userAuth.js
@@ -1,61 +1,62 @@
 const { sendResponse } = require("../Helpers/helpers.commonFunc");
 const jwt = require('jsonwebtoken');
-const Config = require("../Configs/Config.json");
+require('dotenv').config(); // Load environment variables from .env file
+
 const verifyUserMiddleware = (req, res, next) => {
     const token = req.headers["x-access-token"];
     if (!token) {
         sendResponse(res, null, 401, false, "Token not found");
-        return
-    } else {
-        try {
-            const decoded = jwt.verify(token, Config.KEY);
-            if (!decoded) {
-                sendResponse(res, null, 401, false, "Unauthorized Token");
-                return
-            } else {
-                req.user = decoded.user;
-                if (!decoded.isVerified) {
-                    sendResponse(res, null, 401, false, "Invalid Token");
-                    return
-                } else {
-                    next();
-                }
-            }
-        } catch (err) {
-            sendResponse(res, null, 401, false, "Unauthorized Token");
-            return
-        }
+        return;
     }
-}
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        if (!decoded) {
+            sendResponse(res, null, 401, false, "Unauthorized Token");
+            return;
+        }
+
+        req.user = decoded.user;
+        if (!decoded.isVerified) {
+            sendResponse(res, null, 401, false, "Invalid Token");
+            return;
+        }
+
+        next();
+    } catch (err) {
+        sendResponse(res, null, 401, false, "Unauthorized Token");
+        return;
+    }
+};
 
 const verifytUserOtpMiddleware = (req, res, next) => {
     const token = req.headers["x-access-token"];
     if (!token) {
         sendResponse(res, null, 401, false, "Token not found");
-        return
-    } else {
-        try {
-            const decoded = jwt.verify(token, Config.KEY);
-            if (!decoded) {
-                sendResponse(res, null, 401, false, "Unauthorized Token");
-                return
-            } else {
-                req.user = decoded.user;
-                if (!req.user?.isVerified) {
-                    next();
-                } else {
-                    sendResponse(res, null, 401, false, "Invalid Token");
-                    return
-                }
-            }
-        } catch (err) {
-            sendResponse(res, null, 401, false, "Unauthorized Token");
-            return
-        }
+        return;
     }
-}
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        if (!decoded) {
+            sendResponse(res, null, 401, false, "Unauthorized Token");
+            return;
+        }
+
+        req.user = decoded.user;
+        if (!req.user?.isVerified) {
+            next();
+        } else {
+            sendResponse(res, null, 401, false, "Invalid Token");
+            return;
+        }
+    } catch (err) {
+        sendResponse(res, null, 401, false, "Unauthorized Token");
+        return;
+    }
+};
 
 module.exports = {
     verifyUserMiddleware,
-    verifytUserOtpMiddleware
-}
+    verifytUserOtpMiddleware,
+};
