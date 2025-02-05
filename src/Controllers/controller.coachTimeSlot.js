9b66571ab25787799a667e41d134e7bfbc4ff389
AyushDeveloper <debayush2002@gmail.com>
2025-02-05 15:38:56 +0530

    referencing coaches

--- src/Controllers/controller.coachTimeSlot.js
+++ src/Controllers/controller.coachTimeSlot.js
@@ -25,7 +25,7 @@ const getCoachTimeSlotController = async (req, res) => {
 
     try {
         console.log("idhar")
-        console.log(mongoose.Types.ObjectId.isValid(req.params.coachId))
+        console.log(req.params)
         const coachTimeSlot = await getCoachTimeSlotServices(req.params.coachId);
         if (!coachTimeSlot) {
             console.log(" ya idhar")
