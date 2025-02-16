1fa059455c237c2712c68ccac020870884407795
AyushDeveloper <debayush2002@gmail.com>
2025-02-16 18:08:43 +0530

    typos

--- src/Controllers/controller.coachBankDetails.js
+++ src/Controllers/controller.coachBankDetails.js
@@ -1,9 +1,17 @@
 const {createCoachBankDetailsServices}= require('../Services/services.coachBankDetails');
 const { sendResponse } = require("../Helpers/helpers.commonFunc");
-const createCoachBankDetailsController = async (req,res) =>{
-    const coachBankDetails=req.body;
-    await createCoachBankDetailsServices({...coachBankDetails,createdBy:req._id});
-    sendResponse(res,null,200,'Coach Bank Details Created Successfully')
-}
 
-module.exports = {createCoachBankDetailsController}
+const createCoachBankDetailsController = async (req, res) => {
+    try {
+        const coachBankDetails = req.body;
+        console.log("Received request body:", coachBankDetails);
+
+        await createCoachBankDetailsServices({...coachBankDetails, createdBy: req._id});
+        sendResponse(res, null, 200, 'Coach Bank Details Created Successfully');
+    } catch (error) {
+        console.error("Error in createCoachBankDetailsController:", error);
+        sendResponse(res, null, 500, 'Internal Server Error');
+    }
+};
+
+module.exports = createCoachBankDetailsController
