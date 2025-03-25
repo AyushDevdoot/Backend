8e53784ad6a63e556051e802ee5564716d3a6601
Aaryan Raj <itssaaryan@gmail.com>
2025-03-26 02:42:12 +0530

    middleware changes

--- src/Routes/routes.hospital.js
+++ src/Routes/routes.hospital.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getNearbyHospitalsController } = require('../Controllers/controller.hospital');
-const { validateRequest } = require('../Middlewares/validateRequest');
+const { validateRequest } = require('../Middleware/validateRequest');
 const { hospitalSearchDTO } = require('../DTOs/hospital.dto');
 
 router.get(
