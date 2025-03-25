4081d05f619d006e8b7dfde58d4ffbe3553e2fef
arch <mohitrawatssj4@gmail.com>
2025-03-25 17:50:39 +0530

    updated booking processing model

--- src/Routes/routes.booking.js
+++ src/Routes/routes.booking.js
@@ -3,12 +3,12 @@ const { verifyUserMiddleware } = require('../Middleware/userAuth');
 
 const bookingRouter = require('express').Router();
 
-coachAvailabilityRouter.post('/create-booking', verifyUserMiddleware, createAppointmentController);
+bookingRouter.post('/create-booking', verifyUserMiddleware, createAppointmentController);
 
-coachAvailabilityRouter.get('/booking-history', verifyUserMiddleware, getAppointmentHistoryController);
+bookingRouter.get('/booking-history', verifyUserMiddleware, getAppointmentHistoryController);
 
-coachAvailabilityRouter.patch('/payment-status', verifyUserMiddleware, updateAppointmentPaymentStatusController);
+bookingRouter.patch('/payment-status', verifyUserMiddleware, updateAppointmentPaymentStatusController);
 
-coachAvailabilityRouter.patch('/update-booking', verifyUserMiddleware, updateAppointmentController);
+bookingRouter.patch('/update-booking', verifyUserMiddleware, updateAppointmentController);
 
 module.exports = bookingRouter;    
