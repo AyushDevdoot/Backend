3787d50b474830e7eef89e3fda8619c8d6d20826
lightdrk <mohitrawatssj4@gmail.com>
2025-04-02 19:08:40 +0530

    cleared the code

--- src/Models/models.coachInfo.js
+++ src/Models/models.coachInfo.js
@@ -12,38 +12,6 @@ const coachInfoSchema = new mongoose.Schema({
     },
     lastName: {
         type: String,
-<<<<<<< HEAD
-=======
-        enum:[
-            "Addiction Recovery Coach",
-            "Arthritis and Joint Health Coach",
-            "Ayurveda Consultant",
-            "Cardiovascular Health Coach",
-            "Chronic Pain Management Coach",
-            "Dermatologist Consultant",
-            "Detox and Clean Eating Coach",
-            "Diabetes Management Coach",
-            "Health & Fitness Coach",
-            "Holistic Wellness Coach",
-            "Immunity Coach for Kids",
-            "Lifestyle Transformation Coach",
-            "Mental Health Support Coach",
-            "Parenting Wellness Coach",
-            "Post-Surgery Recovery Coach",
-            "Relationship and Couples Coach",
-            "Reproductive Health Coach",
-            "Skin and Beauty Wellness Coach",
-            "Sleep Wellness Coach",
-            "Therapeutic Coach",
-            "Weight Management Coach",
-            "Women’s Health Coach",
-            "Work-Life Balance Coach",
-            "Workplace Stress Coach"
-          ]
-          
-          ,
->>>>>>> origin/main
-        required: true
     },
     specialization: [{
         type: mongoose.Schema.Types.ObjectId,
